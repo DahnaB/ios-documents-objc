5f927afb9fc616aadcfe352d5b572d395d11fcd4
Mitchell Budge <mitchellbudge@gmail.com>
2019-11-06 14:18:14 -0700

    Create initial Xcode project

---
+++ Documents/Documents/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Documents
+//
+//  Created by Mitchell Budge on 11/6/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Documents/Documents/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Documents
+//
+//  Created by Mitchell Budge on 11/6/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
